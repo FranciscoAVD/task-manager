fcfd69f0d5dcc84fb653a1308b45262f89409910
Francisco Victoriano <fvictoriano.dev@gmail.com>
2024-08-12 12:48:47 -0400

    Frank: Seperate section for additional customization in the same section as Table views.

---
+++ src/components/dashboard/actions/dashboard-actions.tsx
@@ -0,0 +1,34 @@
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import {
+  ChevronUp,
+  SearchIcon,
+  SettingsIcon,
+  SlidersHorizontal,
+} from "lucide-react";
+
+const actions = [
+  { icon: SearchIcon, label: "Search" },
+  { icon: SlidersHorizontal, label: "Hide" },
+  { icon: SettingsIcon, label: "Customize" },
+];
+export default function DashboardActions() {
+  return (
+    <div className="flex items-center gap-x-4">
+      <ul className="flex gap-x-2">
+        {actions.map(({ icon: Icon, label }) => (
+          <li key={label}>
+            <Button variant={"ghost"} className="gap-x-2 px-2 rounded-none">
+              <Icon className="size-4" />
+              {label}
+            </Button>
+          </li>
+        ))}
+      </ul>
+      <Separator orientation="vertical" className="h-4 w-[1px]" />
+      <Button size={"icon"} variant={"ghost"} className="bg-neutral-200 w-fit h-fit p-2 rounded-full">
+        <ChevronUp className="size-4" />
+      </Button>
+    </div>
+  );
+}
